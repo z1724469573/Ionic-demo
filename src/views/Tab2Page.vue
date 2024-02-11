53909b084db80e68b2af70577d30de6a77fc6326
筠dayday <1724469573@qq.com>
2024-02-11 10:15:12 +0800

    feat: 实现照片库应用 && 掌握capacitor一些api的用法

---
+++ src/views/Tab2Page.vue
@@ -0,0 +1,49 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 2</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Photo Gallery</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="takePhoto()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery'
+import { camera, trash, close } from 'ionicons/icons'
+import {
+  IonPage,
+  IonHeader,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/vue'
+const { takePhoto, photos } = usePhotoGallery()
+// import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+</script>
